77dafeaa63bcd8edfde2082d0ef65ed3af32e774
Ilia1997 <49026558+Ilia1997@users.noreply.github.com>
2020-12-23 14:03:41 +0200

    add hw12

---
+++ HOMEWORK12/task_1/script.js
@@ -0,0 +1,15 @@
+const http = require("http");
+var url = require("url");
+var static = require("node-static");
+var file = new static.Server(".");
+
+function accept(req, res) {
+  if (req.url == "/info.txt") {
+    file.serve(req, res); // can set delay
+  } else {
+    file.serve(req, res);
+  }
+}
+// ------ run server -------
+http.createServer(accept).listen(8080);
+console.log("Server running on port 8080");
