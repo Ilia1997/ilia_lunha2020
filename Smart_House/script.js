6f5ebacdce6ced00c6782528eed5beb6ca80cafb
Ilia <ilia97ap76@gmail.com>
2020-12-28 18:26:46 +0200

    add func for localStorage

--- Smart_House/script.js
+++ Smart_House/script.js
@@ -1,14 +1,23 @@
-let SmartHouse = require("./smart_house");
+// impart all modules
+const LocalStorageUtil = require("./localStorageUtil");
+const SmartHouse = require("./smart_house");
+const Tv = require("./tv");
 
-// toogle class for main page
-$(document).ready(function () {
-  $(".main_btn_bg").click(function (event) {
-    $(".main_btn_bg, #sidebar,.title,.tit_h1, .head_content_wrap ").toggleClass(
-      "active"
-    );
-    $("body").toggleClass("lock");
-  });
-});
+const fn1 = ".switch_wifi";
+const fn2 = ".switch_wifi span";
+const fn3 = "#wifi_state";
+const fn4 = "Wi-fi вімкнуто";
+const fn5 = "Wi-fi вимкнуто";
+const fn6 = "tv_d_wifi";
+const fn7 = "tv_d_wifi_1";
+
+const en1 = ".switch_eco";
+const en2 = ".switch_eco span";
+const en3 = "#eco_state";
+const en4 = "Економ режим вімкнуто";
+const en5 = "Економ режим вимкнуто";
+const en6 = "tv_d_en";
+const en7 = "tv_d_en_1";
 
 class Light extends SmartHouse {
   //dn for delete()
@@ -21,9 +30,16 @@ class Light extends SmartHouse {
     tn1: ".switch_l",
     tn2: ".switch_l span",
     tn3: ".light_info p",
-    tn4: "Світильник",
+    tn4: "Світильник вімкнено",
+    tn5: "Світильник вимкнено",
+  };
+  rn = {
+    rn1: "light_a",
+    rn2: "light",
+    rn3: "light_a_1",
   };
 }
+
 class Jalousie extends SmartHouse {
   //dn for delete()
   dn = {
@@ -35,22 +51,15 @@ class Jalousie extends SmartHouse {
     tn1: ".switch_j",
     tn2: ".switch_j span",
     tn3: ".jalousie_info p",
-    tn4: "Жалюзі",
+    tn4: "Жалюзі відкриті",
+    tn5: "Жалюзі закриті",
   };
-  // Overriding turnOnAndTurnOff()
-  turnOnAndTurnOff() {
-    let tn1 = this.tn.tn1;
-    let tn2 = this.tn.tn2;
-    let tn3 = this.tn.tn3;
-    let tn4 = this.tn.tn4;
-    $(tn1).click(function () {
-      if ($(tn2).css("background-color") == "rgb(204, 204, 204)") {
-        $(tn3).text(tn4 + " відкриті");
-      } else if ($(tn2).css("background-color") == "rgb(33, 150, 243)") {
-        $(tn3).text(tn4 + " закриті");
-      }
-    });
-  }
+
+  rn = {
+    rn1: "jal_b",
+    rn2: "jalousie",
+    rn3: "jal_b_1",
+  };
 }
 class Warm extends SmartHouse {
   //dn for delete()
@@ -63,144 +72,116 @@ class Warm extends SmartHouse {
     tn1: ".switch_w",
     tn2: ".switch_w span",
     tn3: ".warm_info p",
-    tn4: "Обігрів",
+    tn4: "Обігрів вімкнено",
+    tn5: "Обігрів вимкнено",
+  };
+  rn = {
+    rn1: "warm_c",
+    rn2: "warm",
+    rn3: "warm_c_1",
   };
 }
 
-class Tv extends SmartHouse {
-  //dn for delete()
-  dn = {
-    dn1: ".tv_del_btn_item",
-    dn2: ".tv_comp",
-  };
-  //tn for turnOnAndTurnOff()
-  tn = {
-    tn1: ".switch_tv",
-    tn2: ".switch_tv span",
-    tn3: ".tv_info p",
-    tn4: "Телевізор",
-  };
-  // creating new func for additional parameters Tv class
-
-  whatchParam() {
-    $(".tv_func_icon").click(function () {
-      $(".tv_func_icon, .tv_comp,.tv_comp_items").toggleClass("active");
-      if (
-        $(".switch_tv span").css("background-color") == "rgb(204, 204, 204)"
-      ) {
-        $(".switch_tv span")[0].click();
-      }
-    });
-    $(".list_of_chanel_wrap").click(function () {
-      $(".show_tv_chan, .list_of_chanel_wrap").toggleClass("active");
-    });
-    $(".search_of_chanel_wrap").click(function () {
-      $(".search_info").toggleClass("active");
-      setTimeout(function () {
-        $("#anim").text("Нових каналів не знайдено");
-        $(".loader").css("display", "none");
-      }, 6000);
-    });
-    $(".setting_of_chanel_wrap").click(function () {
-      $(".show_tv_set").toggleClass("active");
-      let date = new Date();
-
-      $(".date_ch input[type='date']").val(
-        date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
-      );
-    });
-  }
-  // creating new func for view tv channels
-  seeChannel() {
-    $(document).on("click", ".all_chan li", function (e) {
-      let lc = $(e.target)[0];
-      const textChan = $(lc).text();
-
-      $(".channel_on_text p").text("Ви дивитесь " + textChan);
-      $(".channel_photo").css(
-        "background",
-        "url(./img/" + textChan + ".jpg) no-repeat center top/ cover"
-      );
-    });
-  }
-  // creating new func for sound change
-  changeSound() {
-    $(".slidecontainer input").on("input", function () {
-      $("#sound_2").text($(".slidecontainer input").val() + "%");
-    });
-  }
-  // creating new func for change wi-fi state
-  changeWifiState() {
-    this.tn.tn1 = ".switch_wifi";
-    this.tn.tn2 = ".switch_wifi span";
-    this.tn.tn3 = "#wifi_state";
-    this.tn.tn4 = "Wi-fi";
-    return this.turnOnAndTurnOff();
-  }
-  // creating new func for change economy mode state
-  changeEconomyModeState() {
-    this.tn.tn1 = ".switch_eco";
-    this.tn.tn2 = ".switch_eco span";
-    this.tn.tn3 = "#eco_state";
-    this.tn.tn4 = "Економ режим";
-    return this.turnOnAndTurnOff();
-  }
-  // creating new func for change state li items with id = state
-  changeStateLiItems() {
-    $(document).on("click", "#state li", function (e) {
-      let elem = $(e.target)[0];
-      const className = $(elem).parent().attr("class");
-      $("." + className)
-        .children()
-        .removeClass("active");
-      $(this).addClass("active");
-    });
-  }
-
-  // creating new func for show all settings
-  showSetting() {
-    $(document).on("click", ".all_set li", function (e) {
-      let elem = $(e.target)[0];
-      const textChan = $(elem).text();
-      const idElem = $(elem).attr("id");
-
-      $(".channel_set_text p").text("Ви налаштовуєте " + textChan);
-      $(".channel_show_set").children().css("display", "none");
-      $("." + idElem).css("display", "flex");
-    });
-  }
-  // Overriding delete()
-  delete() {
-    let dn1 = this.dn.dn1;
-    let dn2 = this.dn.dn2;
-
-    $(dn1).click(function () {
-      $(dn2).remove();
-      $(".tv_comp_items").css("display", "none");
-    });
-  }
-}
-
-// сreate classes and activate methods
 const light = new Light();
-light.turnOnAndTurnOff();
 light.delete();
 const jalousie = new Jalousie();
-jalousie.turnOnAndTurnOff();
 jalousie.delete();
 const warm = new Warm();
-warm.turnOnAndTurnOff();
 warm.delete();
 const tv = new Tv();
-tv.turnOnAndTurnOff();
 tv.delete();
 tv.whatchParam();
 tv.seeChannel();
 tv.changeSound();
-tv.changeWifiState();
 tv.changeStateLiItems();
 tv.showSetting();
-tv.changeEconomyModeState();
-
 const sh = new SmartHouse();
 sh.create();
+const localStorageUtil = new LocalStorageUtil();
+// func for main button
+$(".main_btn_bg").click(function (event) {
+  $(".main_btn_bg, #sidebar,.title,.tit_h1, .head_content_wrap ").toggleClass(
+    "active"
+  );
+
+  // for localStorage work
+  let array_obj = [light, jalousie, warm, tv];
+
+  for (let i in array_obj) {
+    let tn1 = array_obj[i].tn.tn1;
+    let tn2 = array_obj[i].tn.tn2;
+    let ln3 = array_obj[i].rn.rn3;
+
+    let array = localStorageUtil.getBtns();
+    console.log(array);
+    for (let t in array) {
+      console.log(array[t]);
+      if (array[t] === ln3) {
+        $(tn1 + " input").attr("checked", "checked");
+
+        array_obj[i].turnOn();
+        $(tn2).attr("id", ln3);
+      }
+    }
+    for (let r in array) {
+      if (array[r] === fn7) {
+        $(fn1 + " input").attr("checked", "checked");
+        if ($(fn2).css("background-color") == "rgb(33, 150, 243)") {
+          $(fn3).text(fn4);
+        }
+        $(fn2).attr("id", fn7);
+      }
+    }
+
+    for (let eco in array) {
+      if (array[eco] === en7) {
+        $(en1 + " input").attr("checked", "checked");
+        if ($(en2).css("background-color") == "rgb(33, 150, 243)") {
+          $(en3).text(en4);
+        }
+        $(en2).attr("id", en7);
+      }
+    }
+  }
+  $("body").toggleClass("lock");
+});
+// click on check box
+$(document).on("click", function (e) {
+  let elem = $(e.target)[0];
+  console.log(elem);
+
+  let array_obj = [light, jalousie, warm, tv];
+  for (let i in array_obj) {
+    let ln1 = array_obj[i].rn.rn1;
+    let ln3 = array_obj[i].rn.rn3;
+
+    if ($(elem).attr("id") === ln1) {
+      array_obj[i].turnOn();
+      $(elem).attr("id", ln3);
+      localStorageUtil.setBtns($(elem).attr("id"));
+    } else if ($(elem).attr("id") === ln3) {
+      array_obj[i].turnOff();
+
+      localStorageUtil.setBtns($(elem).attr("id"));
+      $(elem).attr("id", ln1);
+    }
+  }
+  if ($(elem).attr("id") === fn6) {
+    tv.changeWifiTurnOn(fn4);
+
+    $(elem).attr("id", fn7);
+    localStorageUtil.setBtns($(elem).attr("id"));
+  } else if ($(elem).attr("id") === fn7) {
+    tv.changeWifiTurnOff(fn5);
+    localStorageUtil.setBtns($(elem).attr("id"));
+    $(elem).attr("id", fn6);
+  } else if ($(elem).attr("id") === en6) {
+    tv.changeEcoTurnOn(en4);
+    $(elem).attr("id", en7);
+    localStorageUtil.setBtns($(elem).attr("id"));
+  } else if ($(elem).attr("id") === en7) {
+    tv.changeEcoTurnOff(en5);
+    localStorageUtil.setBtns($(elem).attr("id"));
+    $(elem).attr("id", en6);
+  }
+});
